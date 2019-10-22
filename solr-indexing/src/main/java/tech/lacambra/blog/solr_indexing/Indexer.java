7e323c8fcb23c5774b50364a3462f77be1495ddd
Albert Lacambra Basil <albert@lacambra.tech>
2019-10-22 18:28:31 +0200

    configuring solr

--- solr-indexing/src/main/java/tech/lacambra/blog/solr_indexing/Indexer.java
+++ solr-indexing/src/main/java/tech/lacambra/blog/solr_indexing/Indexer.java
@@ -14,34 +14,37 @@ public class Indexer {
   public static void main(String[] args) throws IOException, SolrServerException {
 
     Indexer indexer = new Indexer();
-    String collection = "configset-ex";
-//    String collection = "blog-solr";
+//    String collection = "configset-ex";
+    String collection = "blog-solr";
 
     final SolrInputDocument doc = new SolrInputDocument();
     String id = "id1";
     doc.addField("id", id);
     doc.addField("title", "Amazon Kindle Paperwhite");
+    doc.addField("text", "lalala");
+    doc.addField("reducedText", "lalala2");
 
-    HttpSolrClient client = indexer.getClient();
-
-    final UpdateResponse updateResponse = client.add(collection, doc);
+    try (HttpSolrClient client = indexer.getClient()) {
+      UpdateResponse updateResponse = client.add(collection, doc);
 // Indexed documents must be committed
-    client.commit(collection);
+      client.commit(collection);
 
-    final SolrQuery query = new SolrQuery("*:*");
-    query.addField("id");
+      final SolrQuery query = new SolrQuery("lalala2");
+      query.addField("id");
 //    query.addField("_text_");
-    query.addField("text");
-    query.setSort("id", SolrQuery.ORDER.asc);
+      query.addField("text");
+      query.addField("reducedText");
+      query.setSort("id", SolrQuery.ORDER.asc);
 
-    final QueryResponse response = client.query(collection, query);
-    System.out.println(response);
+      final QueryResponse response = client.query(collection, query);
+      System.out.println(response);
+    }
 
   }
 
   private HttpSolrClient getClient() {
-//    final String solrUrl = "http://solr-blog-blog.apps.oc.lacambra/solr";
-    final String solrUrl = "http://solr-demo-blog.apps.oc.lacambra/solr";
+    final String solrUrl = "http://solr-blog-blog.apps.oc.lacambra/solr";
+//    final String solrUrl = "http://solr-demo-blog.apps.oc.lacambra/solr";
     return new HttpSolrClient.Builder(solrUrl)
         .withConnectionTimeout(10000)
         .withSocketTimeout(60000)
